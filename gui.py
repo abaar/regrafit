be906eddf96ae91dab2683ba87f4bf71705de0ed
Ayas F.N <ayasfn@gmail.com>
2018-06-20 15:20:16 +0700

    fixed line duplicate, partial delete line

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import scrolledtext as tkst
 from draggable import Draggable
+import math
 
 class Gui(ttk.Frame):
     def __init__(self, master=None):
@@ -87,7 +88,6 @@ class Gui(ttk.Frame):
             vtag = self.currentvertex
             if vtag != False:
                 label = self.secondcanvas.find_withtag(vtag)[1]
-                self.log(self.secondcanvas.coords(label))
                 xline, yline = self.secondcanvas.coords(label)
                 if self.secondcanvas.find_withtag('ln'+str(self.lineNum)):
                     self.secondcanvas.coords('ln'+str(self.lineNum),xline,yline, x, y)
@@ -103,9 +103,6 @@ class Gui(ttk.Frame):
                 lo = self.secondcanvas.find_withtag('lo'+vtag)
                 self.secondcanvas.coords(v[0],x+r, y+r, x-r,y-r)
                 self.secondcanvas.coords(v[1],x, y)
-                self.log('vtag '+str(v))
-                self.log('li '+str(li))
-                self.log('lo '+str(lo))
                 for i in lo:
                     linecoord = self.secondcanvas.coords(i)
                     self.secondcanvas.coords(i,x,y,linecoord[2],linecoord[3])
@@ -118,16 +115,33 @@ class Gui(ttk.Frame):
         cekmode = self.mode
         if cekmode == 'add':
             vtag = self.isIntersect(event.x,event.y)
-            if vtag != False and vtag != self.currentvertex:
+            curvtex= self.currentvertex
+            cekline = self.secondcanvas.find_withtag('ln'+str(self.lineNum))
+           
+            if vtag != False and vtag != curvtex and cekline:
                 label = self.secondcanvas.find_withtag(vtag)[1]
                 self.log(self.secondcanvas.coords(label))
                 xver, yver = self.secondcanvas.coords(label)
 
-                if self.secondcanvas.find_withtag('ln'+str(self.lineNum)):
-                    linecoord = self.secondcanvas.coords('ln'+str(self.lineNum))
-                    self.secondcanvas.coords('ln'+str(self.lineNum),linecoord[0],linecoord[1],xver,yver)
-                    self.secondcanvas.addtag_withtag('li'+vtag,'ln'+str(self.lineNum))
+                linecoord = self.secondcanvas.coords('ln'+str(self.lineNum))
+                self.secondcanvas.coords('ln'+str(self.lineNum),linecoord[0],linecoord[1],xver,yver)
+                self.secondcanvas.addtag_withtag('li'+vtag,'ln'+str(self.lineNum))
+                counter = 0
+                for i in self.secondcanvas.find_withtag('line'):
+                    linetag = self.secondcanvas.gettags(i)
+                    if ('li'+curvtex in linetag or 'lo'+curvtex in linetag) and ('li'+vtag in linetag or 'lo'+vtag in linetag):
+                        counter+=1
+                        if counter>1:
+                            self.secondcanvas.delete('ln'+str(self.lineNum))
+                            self.log('gabisa lagii')
+                            
+                if counter==1:
+                    self.log('berhasil')
                     self.lineNum +=1
+                self.log('jumlah line '+str(self.lineNum-1))
+            else:
+                self.secondcanvas.delete('ln'+str(self.lineNum))
+                self.log('gagal')
 
         vtag  = self.currentvertex
         if vtag != False:
@@ -137,7 +151,11 @@ class Gui(ttk.Frame):
     
     def cRight(self, event):
         x, y = event.x, event.y
-        self.delVertex(x,y)
+        isLine = self.isIntersect(x,y,'line')
+        if isLine:
+            self.delLine(x,y,isLine)
+        else:    
+            self.delVertex(x,y)
 
     def drawVertex(self, x, y):
         if self.isIntersect(x,y) == False:
@@ -155,7 +173,6 @@ class Gui(ttk.Frame):
         if vtag != False:
             v = self.secondcanvas.find_withtag(vtag)
             self.secondcanvas.itemconfigure(v[0],outline = 'red')
-
     
     def delVertex(self, x, y):
         vtag = self.isIntersect(x,y)
@@ -164,23 +181,50 @@ class Gui(ttk.Frame):
             self.secondcanvas.delete(vtag)
             self.secondcanvas.delete('li'+vtag)
             self.secondcanvas.delete('lo'+vtag)
+            self.lineNum-=1
+        
+    def delLine(self,x,y,linetag):
+        if linetag != False:
+            self.log('line '+str(linetag)+' deleted')
+            self.secondcanvas.delete(linetag)
+            self.secondcanvas.dtag('vl'+linetag,)
+            self.secondcanvas.dtag('vl'+linetag)
 
-    
-    def isIntersect(self, x, y):
-        allver = self.secondcanvas.find_withtag('circle')
-        # self.log(str(allver))
+    def isIntersect(self, x, y,obj='circle'):
+        allver = self.secondcanvas.find_withtag(obj)
         self.log('current pos '+str(x)+' '+str(y))
-        for i in allver:
-            x1,y1,x2,y2 = self.secondcanvas.coords(i)
-            if x>=x1 and y >= y1 and x<=x2 and y<=y2:
-                vtag = self.secondcanvas.gettags(i)[0]
-                return vtag
-            # self.log(str(x1)+' '+str(y1)+' '+str(x2)+' '+str(y2))
+        if obj == 'circle':
+            for i in allver:
+                x1,y1,x2,y2 = self.secondcanvas.coords(i)
+                if x>=x1 and y >= y1 and x<=x2 and y<=y2:
+                    vtag = self.secondcanvas.gettags(i)[0]
+                    return vtag
+                # self.log(str(x1)+' '+str(y1)+' '+str(x2)+' '+str(y2))
+        elif obj == 'line':
+            for i in allver:
+                x1,y1,x2,y2 = self.secondcanvas.coords(i)
+                if x>=x1 and y >= y1 and x<=x2 and y<=y2:
+                    vtag = self.secondcanvas.gettags(i)[0]
+                    return vtag
+                self.log(str(x1)+' '+str(y1)+' '+str(x2)+' '+str(y2))
+
+        return False
+
+    def isBetween(self,x,y,x1,y1,x2,y2):
+        if x>=x1 and y >= y1 and x<=x2 and y<=y2:
+            return True
+        elif x<=x1 and y <= y1 and x>=x2 and y>=y2:
+            return True
+        elif x>=x1 and y <= y1 and x<=x2 and y>=y2:
+            return True
         return False
 
     def delAll(self, *args):
         self.secondcanvas.delete("all")
         self.log('',True)
+        self.vertexNum = 1
+        self.lineNum = 1
+        self.currentvertex = False
 
     def getComboVal(self):
         return self.dropval.get()
